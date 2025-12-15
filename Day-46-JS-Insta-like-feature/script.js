01341c68b735b9e8d2739a459012a9bfeb710517
Ritik Kumar <ritik.kumar22@outlook.com>
2025-12-15 13:26:43 +0530

    Day-46 assignment added

---
+++ Day-46-JS-Insta-like-feature/script.js
@@ -0,0 +1,19 @@
+let img = document.querySelector("img");
+let love = document.querySelector("#love");
+
+img.addEventListener("dblclick", function() {
+    love.style.opacity = 1;
+    love.style.transform = "translate(-50%, -50%) scale(1) rotate(0deg)";
+
+    setTimeout(() => {
+        love.style.transform = "translate(-50%, -400%) scale(1) rotate(60deg)";
+    }, 800);
+
+    setTimeout(() => {
+        love.style.opacity = 0;
+    }, 1000);
+
+    setTimeout(() => {
+        love.style.transform = "translate(-50%, -50%) scale(0) rotate(-60deg)";
+    }, 1200);
+});
